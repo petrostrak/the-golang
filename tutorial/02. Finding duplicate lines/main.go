2a35eb1657f573ac205694cf91fded8ea19b50b3
petros <pit.trak@gmail.com>
2021-01-24 18:06:36 +0200

    finding duplicate lines

--- tutorial/02. Finding duplicate lines/main.go
+++ tutorial/02. Finding duplicate lines/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func dup1() {
@@ -51,6 +53,25 @@ func countLines(f *os.File, counts map[string]int) {
 	}
 }
 
+func dup3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s", n, line)
+		}
+	}
+}
+
 func main() {
-	dup2()
+	dup3()
 }
