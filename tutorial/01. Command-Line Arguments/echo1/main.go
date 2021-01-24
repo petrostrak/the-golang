65b38063a24e2dd6be68f4a58ed4446ec9ed3dbb
petros <pit.trak@gmail.com>
2021-01-24 16:53:15 +0200

    impl of echo

--- tutorial/01. Command-Line Arguments/echo1/main.go
+++ tutorial/01. Command-Line Arguments/echo1/main.go
@@ -4,13 +4,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func main() {
+func echo1() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
-		sep = ""
+		sep = " "
 	}
 	fmt.Println(s)
+}
+
+func echo2() {
+	s, sep := "", ""
+	for i, args := range os.Args[1:] {
+		s += sep + args
+		sep = " "
+		fmt.Println(i)
+	}
+	fmt.Println(s)
+}
+
+func echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func echo4() {
+	fmt.Println(os.Args[0:])
+}
+
+func main() {
+	echo2()
 }
