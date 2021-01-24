04b351f770c5e6203c8484d1e08468737ef5c39a
petrostrak <pit.trak@gmail.com>
2021-01-24 22:07:31 +0200

    animated gifs

--- tutorial/03. Animated gifs/main.go
+++ tutorial/03. Animated gifs/main.go
@@ -37,8 +37,8 @@ func lissajous(out io.Writer) {
 		img := image.NewPaletted(rect, palete)
 
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
+			x := math.Cos(t)
+			y := math.Cos(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
 		phase += 0.1
