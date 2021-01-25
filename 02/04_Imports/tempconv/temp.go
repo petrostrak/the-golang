5a1e419f73e76836ef2f45a0c23d432f385ca435
petros <pit.trak@gmail.com>
2021-01-25 18:13:48 +0200

    Imports

---
+++ 02/04_Imports/tempconv/temp.go
@@ -0,0 +1,14 @@
+package tempconv
+
+type Celsius float64
+type Fahrenheit float64
+
+// CToF converts Celsius to Fahrenheit
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+// FToC converts Fahrenheit to Celsius
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
