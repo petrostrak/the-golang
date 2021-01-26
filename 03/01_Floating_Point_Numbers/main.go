eee251331c6e888e2ff8984e3155b79f875422cf
petros <pit.trak@gmail.com>
2021-01-26 16:36:56 +0200

    Rendered svg image on server

--- 03/01_Floating_Point_Numbers/main.go
+++ 03/01_Floating_Point_Numbers/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"math"
+	"net/http"
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
+	width, height = 1200, 640           // canvas size in pixels
 	cells         = 100                 // number of grid cells
 	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
@@ -16,8 +17,9 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
-func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+func handleSvg(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
@@ -26,11 +28,16 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Fprintf(w, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintln(w, "</svg>")
+}
+
+func main() {
+	http.HandleFunc("/", handleSvg)
+	http.ListenAndServe(":8000", nil)
 }
 
 func corner(i, j int) (float64, float64) {
