283f7f1010bcdfe038d9aa3c24941d1fe43850e5
petros <pit.trak@gmail.com>
2021-01-26 21:18:58 +0200

    Insert comma every n characters

--- 03/03_Strings_and_Byte_Slices/comma.go
+++ 03/03_Strings_and_Byte_Slices/comma.go
@@ -3,7 +3,10 @@ package main
 func comma(s string) string {
 	n := len(s)
 	if n < 3 {
-		return s
+		return s // no comma is necessary
 	}
+	// calls itself recursively with a substring of all but
+	// the last 3 characters, appends the comma and the last
+	// 3 characters
 	return comma(s[:n-3]) + "," + s[n-3:]
 }
