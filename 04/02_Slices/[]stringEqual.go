0c57246e12fbec5fbeddccd65189d2051727d9c5
petros <pit.trak@gmail.com>
2021-01-27 17:01:07 +0200

    Compare string slices

---
+++ 04/02_Slices/[]stringEqual.go
@@ -0,0 +1,15 @@
+package main
+
+// Equal receives two slices of string
+// and returns true if they are equal
+func Equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
