f28e00e99bdea1c51171c2dc216f6c1317b5f0ca
petros <pit.trak@gmail.com>
2021-01-28 17:07:34 +0200

    equal Maps func

---
+++ 04/04_Maps/equalMaps.go
@@ -0,0 +1,13 @@
+package main
+
+func equalMaps(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
