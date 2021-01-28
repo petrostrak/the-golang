413919b85dd29cd11b90653bb5ec4057dab16f7e
petros <pit.trak@gmail.com>
2021-01-28 10:24:15 +0200

    remove element from slice

--- 04/02_Slices/nonempty.go
+++ 04/02_Slices/nonempty.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func nonEmpty(str []string) []string {
 	i := 0
 	for _, s := range str {
@@ -15,8 +17,21 @@ func nonEmptyAlt(str []string) []string {
 	out := str[:0] // creating an zero-length slice of original
 	for _, s := range str {
 		if s != "" {
-			out = append(out, s)
+			out = append(out, s) // push s
 		}
 	}
 	return out
+}
+
+// removeElem removes an element from the middle of the
+// given slice, preserving the order of the remaining
+// elements
+func removeElem(slice []int, i int) []int {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+
+func main() {
+	a := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(removeElem(a, 3))
 }
--- 04/02_Slices/reverse.go
+++ 04/02_Slices/reverse.go
@@ -1,23 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func reverse(s []int) {
 	for i := 0; i <= len(s)-1; i++ {
 		for j := 0; j < i; j++ {
 			s[i], s[j] = s[j], s[i]
 		}
 	}
-}
-
-func main() {
-	a := [...]int{0, 1, 2, 3, 4, 5}
-	b := []string{"hello"}
-	c := []string{"world"}
-	// d := []string{"hello", "world"}
-	reverse(a[:]) // [:] referes to the whole array
-	fmt.Println(a)
-	fmt.Println(Equal(b, c))
 }
