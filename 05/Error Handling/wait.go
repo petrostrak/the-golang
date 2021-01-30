314c7131edad8629ee5d3513cda6e3b221fe4e18
petrostrak <pit.trak@gmail.com>
2021-01-30 22:26:27 +0200

    exponential back off

---
+++ 05/Error Handling/wait.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// WaitForServer attempts to contract the server
+// of a URL. It tries for one minute using exponential
+// back-off. It reports an arror if all attempts fail
+func WaitForServer(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
+		if err == nil {
+			return nil // success
+		}
+		log.Printf("server not responding (%s); retrying...", err)
+		time.Sleep(time.Second << uint(tries)) // exponential back-off
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
+}
