4c96a071769ddb8685aa5d0b8e315ed3d60eba22
petros <pit.trak@gmail.com>
2021-01-31 11:14:46 +0200

    EOF

---
+++ 05/Error Handling/eof.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// EOF is the error returned by Read when no more input is
+// available.
+var EOF = errors.New("EOF")
+
+func endOfFile() error {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := in.ReadRune()
+		if err == io.EOF {
+			break // finished reading
+		}
+		if err != nil {
+			return fmt.Errorf("read failed: %v\n", err)
+		}
+	}
+	return nil
+}
