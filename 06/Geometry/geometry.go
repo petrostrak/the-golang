b1a07cc6b1f57cdcc0227afa3c77a86980e4b031
petros <pit.trak@gmail.com>
2021-02-01 15:10:52 +0200

    methods associated to any type

--- 06/Geometry/geometry.go
+++ 06/Geometry/geometry.go
@@ -10,6 +10,9 @@ type Point struct {
 	X, Y float64
 }
 
+// Path type
+type Path []Point
+
 // Distance return the Hopot of two points
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
@@ -20,9 +23,29 @@ func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+// Distance returns the distance traveled along the path
+// We associate methods to any type
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
 func main() {
 	p := Point{2, 4}
 	q := Point{6, 8}
 	fmt.Println(Distance(p, q)) //function call
 	fmt.Println(p.Distance(q))  // method call
+
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
 }
