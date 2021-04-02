01475ba2eb6ff55f982c78623fbb208458f9682a
petros <pit.trak@gmail.com>
2021-04-02 12:54:50 +0300

    unidirectional channel type

---
+++ 08/pipelines2/pipeline2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- int(math.Pow(float64(v), 2))
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go counter(naturals)
+	go squarer(naturals, squares)
+	go printer(squares)
+}
